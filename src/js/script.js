1bc24cf2677f4b8e81a978cc1c1d3ce035bc8cbf
Denis <gabbas.denn@mail.ru>
2022-08-28 12:44:47 +0300

    start reliztion slider using different methods

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000
+      });
+});
+                  
