a5019e3e25c91d0ef7dacb2ccd3a53c54f92a1db
Denis <gabbas.denn@mail.ru>
2022-09-10 18:06:49 +0300

    finish realization slider wint tiny slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,36 @@
 
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        adaptiveHeight: true,
-        autoplay: true,
-        autoplaySpeed: 2000
-      });
+// $(document).ready(function(){
+// 	$('.carousel__inner').slick({
+// 			speed: 1200,
+// 			// adaptiveHeight: true,
+// 			nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron_right_solid.png"></button>',
+// 			prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron_left_solid.png"></button>',
+// 			responsive: [
+// 				{
+// 					breakpoint: 993,
+// 					settings: {
+// 						dots: true,
+// 						arrows: false,
+// 					}
+// 				}
+// 			]
+// 		});
+// });
+               
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+	controls: false,
+	nav: false
 });
-                  
+
+document.querySelector('.prev').addEventListener('click', function () {
+	slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+	slider.goTo('next');
+});
